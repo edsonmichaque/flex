d43075327699725f8704edbddf1e7d6407f08b7f
Edson Michaque <edson@michaque.com>
2020-06-24 05:58:52 +0200

    Add tests for vector_new, vector_distance and vector_distance functions

--- include/flex2d/vector.h
+++ include/flex2d/vector.h
@@ -1,6 +1,3 @@
-#ifndef __VECTOR_H__
-#define __VECTOR_H_
-
 typedef struct Vector_t Vector;
 typedef struct Vector Point;
 
@@ -11,6 +8,4 @@ struct Vector_t
 
 Vector *vector_new(double, double);
 double vector_distance_squared(Vector *);
-double distance(Vector *);
-
-#endif /* __VECTOR_H__ */
+double vector_distance(Vector *);

f04a99c82a5c575984f1ab5dc263b5233eb989fd
Edson Michaque <edson@michaque.com>
2020-10-17 00:27:00 +0200

    Impement Vector::distance, Vector::distance_squared, Vector::magnitude and Vector::magnitude_squared

---
+++ src/vector.rs
@@ -0,0 +1,79 @@
+#[derive(Debug)]
+pub struct Vector {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Vector {
+    pub fn new(x: f64, y: f64) -> Self {
+        Vector {
+            x,
+            y,
+        }
+    }
+
+    pub fn distance_squared(&self, other: &Vector) -> f64 {
+        (self.x - other.x).powi(2) + (self.y - other.y).powi(2)
+    }
+
+    pub fn distance(&self, other: &Vector) -> f64 {
+        self.distance_squared(other).sqrt()
+    }
+
+    pub fn magnitude_squared(&self) -> f64 {
+        self.x.powi(2) + self.y.powi(2)
+    }
+
+    pub fn magnitude(&self) -> f64 {
+        self.magnitude_squared().sqrt()
+    }
+}
+
+#[cfg(test)]
+mod vector_test {
+    use super::Vector;
+
+    #[test]
+    fn vector_magnitude_squared() {
+        let p0x = 1.0f64;
+        let p0y = 1.0f64;
+        let p0 = Vector::new(p0x, p0y);
+
+        assert_eq!(p0.magnitude_squared(), p0x*p0x + p0y*p0y);
+    }
+
+    #[test]
+    fn vector_magnitude() {
+        let p0x = 1.0f64;
+        let p0y = 1.0f64;
+        let p0 = Vector::new(p0x, p0y);
+
+        assert_eq!(p0.magnitude(), (p0x*p0x + p0y*p0y).sqrt());
+    }
+
+    #[test]
+    fn vector_distance_squared() {
+        let p0x = 1.0f64;
+        let p0y = 1.0f64;
+        let p0 = Vector::new(p0x, p0y);
+        
+        let p1x = 1.0f64;
+        let p1y = 1.0f64;
+        let p1 = Vector::new(p1x, p1y);
+
+        assert_eq!(p0.distance_squared(&p1), (p0x-p1x).powi(2) + (p0y-p1y).powi(2));
+    }
+
+    #[test]
+    fn vector_distance() {
+        let p0x = 1.0f64;
+        let p0y = 1.0f64;
+        let p0 = Vector::new(p0x, p0y);
+        
+        let p1x = 1.0f64;
+        let p1y = 1.0f64;
+        let p1 = Vector::new(p1x, p1y);
+
+        assert_eq!(p0.distance_squared(&p1), ((p0x-p1x).powi(2) + (p0y-p1y).powi(2)).sqrt());
+    }
+}
